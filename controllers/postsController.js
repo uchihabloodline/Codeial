2a6041ae696e500b10f8bfdf9af4b863336ce4c5
shivam <geetapandey595@gmail.com>
2020-04-09 02:42:57 +0530

    jquery added

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,14 +3,23 @@ const Comment = require('../model/comment');
 
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user:   req.user._id,
         })
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post: post,
+                },
+                    message: "post created!",
+            });
+        }
+
         return res.redirect('back');
     }catch(err){
         console.log("ERROR--> ",err);
-        return;
+        return res.redirect('back');
     }
 };
 
