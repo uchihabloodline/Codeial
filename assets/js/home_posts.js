25aaae342a527426ca7b6164440423d6ca4d64b3
shivam <geetapandey595@gmail.com>
2020-04-12 02:24:13 +0530

    post delete ajax added

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -53,6 +53,24 @@
                     
                 </li>`)
     }
+ // method to delete a post from DOM
+ let deletePost = function(deleteLink){
+    $(deleteLink).click(function(e){
+        e.preventDefault();
+
+        $.ajax({
+            type: 'get',
+            url: $(deleteLink).prop('href'),
+            success: function(data){
+                $(`#post-${data.data.post_id}`).remove();
+            },error: function(error){
+                console.log(error.responseText);
+            }
+        });
+
+    });
+}
+
 
     createPost();
 }
--- controllers/postsController.js
+++ controllers/postsController.js
@@ -15,7 +15,8 @@ module.exports.create = async function(req,res){
                     message: "post created!",
             });
         }
-
+        
+        req.flash('success', 'Post published!');
         return res.redirect('back');
     }catch(err){
         console.log("ERROR--> ",err);
@@ -31,6 +32,17 @@ module.exports.destroy = async function(req,res){
             post.remove();
             
         await Comment.deleteMany({post: req.params.id});
+        //AJAX used for receiving post-id for deletion
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post_id: req.params.id
+                },
+                message: "Post deleted"
+            });
+        }
+        req.flash('success', 'Post and associated comments deleted!');
+
         return res.redirect('back');
         }else{
             return res.redirect('back')
